11373f4be5dd942fea0230690bd6c7dae8469270
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2023-07-26 11:00:48 +0700

    Added caching

--- src/main/java/me/hungaz/proxyshield/proxyshield/ProxyShield.kt
+++ src/main/java/me/hungaz/proxyshield/proxyshield/ProxyShield.kt
@@ -5,17 +5,18 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.configuration.file.FileConfiguration
+import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.InetAddress
 import java.net.URL
-import org.json.JSONObject
-import java.io.File
 
 class ProxyShield : JavaPlugin(), Listener {
     private lateinit var config: FileConfiguration
+    private val proxyCache: MutableMap<String, Int> = HashMap()
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
@@ -36,18 +37,19 @@ class ProxyShield : JavaPlugin(), Listener {
     private fun getPublicIPAddress(): String? {
         try {
             val url = URL("https://api.ipify.org?format=json")
-            val connection = url.openConnection()
+            val connection = url.openConnection() as HttpURLConnection
             connection.connectTimeout = 5000
             connection.readTimeout = 5000
             connection.connect()
 
-            val reader = connection.getInputStream().bufferedReader()
+            val reader = connection.inputStream.bufferedReader()
             val response = reader.readLine().trim()
             reader.close()
 
             val json = JSONObject(response)
             return json.getString("ip")
         } catch (e: IOException) {
+            logger.warning("Failed to get the public IP address of the server.")
             e.printStackTrace()
             return null
         }
@@ -77,6 +79,24 @@ class ProxyShield : JavaPlugin(), Listener {
             return
         }
 
+        val block = performProxyCheck(playerIp)
+        if (block == 1) {
+            val kickMessage = config.getString("kick_message") ?: "VPN/Proxies aren't allowed on this server. Please consider disabling it."
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickMessage)
+            logger.info("$playerName's IP has been flagged as a Proxy")
+        } else {
+            val showPassedMessage = config.getBoolean("show_passed_check_message", true)
+            if (showPassedMessage) {
+                logger.info("$playerName has passed the VPN/Proxy check.")
+            }
+        }
+    }
+
+    private fun performProxyCheck(playerIp: String): Int {
+        if (proxyCache.containsKey(playerIp)) {
+            return proxyCache[playerIp]!!
+        }
+
         val apiKey = config.getString("api_key", "YOUR_API_KEY")
         val requestUrl = "http://v2.api.iphub.info/ip/$playerIp?key=$apiKey"
 
@@ -92,23 +112,23 @@ class ProxyShield : JavaPlugin(), Listener {
             when (connection.responseCode) {
                 403 -> {
                     logger.severe("Invalid API Key, please take a look at your config file sir.")
-                    return
+                    return -1
                 }
                 422 -> {
                     logger.severe("The IPHub API cannot process the request. Check your API key and request URL.")
-                    return
+                    return -1
                 }
                 429 -> {
                     logger.severe("You've exceeded your API Key available queries.")
-                    return
+                    return -1
                 }
                 502 -> {
                     logger.severe("Looks like the IPHub servers are having trouble, IPs will be ignored.")
-                    return
+                    return -1
                 }
                 503 -> {
                     logger.severe("IPHub servers are overloaded or down; IPs will be ignored.")
-                    return
+                    return -1
                 }
             }
 
@@ -117,19 +137,13 @@ class ProxyShield : JavaPlugin(), Listener {
             }
             val json = JSONObject(response)
             val block = json.getInt("block")
-            if (block == 1) {
-                val kickMessage = config.getString("kick_message") ?: "VPN/Proxies aren't allowed on this server. Please consider disabling it."
-                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickMessage)
-                logger.info("$playerName's IP has been flagged as a Proxy")
-                return
-            } else {
-                val showPassedMessage = config.getBoolean("show_passed_check_message", true)
-                if (showPassedMessage) {
-                    logger.info("$playerName has passed the VPN/Proxy check.")
-                }
-            }
+
+            proxyCache[playerIp] = block
+            return block
         } catch (e: IOException) {
+            logger.warning("Failed to perform the Proxy check for IP: $playerIp")
             e.printStackTrace()
         }
+        return -1
     }
 }
